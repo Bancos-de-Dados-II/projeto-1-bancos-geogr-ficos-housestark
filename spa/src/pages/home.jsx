cf2472f137113a1af218b40c1eca386891ac7c39
José Henrique <jh4832184@gmail.com>
2024-12-09 22:04:13 -0300

    refactor: standardizing pages

--- spa/src/pages/home.jsx
+++ spa/src/pages/home.jsx
@@ -5,26 +5,25 @@ import { NavLink } from 'react-router'
 
 export function Home() {
   return (
-    <div className='bg-slate-300 h-screen p-4 grid grid-cols-2'>
-      <main className='bg-slate-50 p-4 mr-4 rounded-xl'>
-        <div className='flex justify-between'>
-          <h1 className='text-2xl font-bold'>Family Farming</h1>
+    <div className='bg-slate-300 h-screen p-8 grid grid-cols-2 gap-8'>
+      <main className='bg-slate-50 p-8 rounded-xl'>
+        <h1 className='text-2xl font-bold mb-4'>Family Farming</h1>
+        <div className='flex justify-between items-center mb-4 mt-4'>
+          <h1 className='text-lg font-medium text-slate-700'>Criar agricultor</h1>
           <NavLink to="/agricultores">
-            <Button>Lista</Button>
+            <Button>Lista</Button> 
           </NavLink>
         </div>
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-4 mb-4'>
           <Input placeholder="Digite o nome" />
-          <Input placeholder="Digite o nome" />
-          <Input placeholder="Digite o nome" />
-          <Input placeholder="Digite o nome" />
-          <Input placeholder="Digite o nome" />
+          <Input placeholder="Digite o email" />
+          <Input placeholder="Digite o telefone" />
+          <Input placeholder="Digite o tamanho do terreno" />
         </div>
         <Button>Salvar</Button>
-        <p></p>
       </main>
       <div className="min-h-[50vh] rounded-xl">
-        <Map/>
+        <Map />
       </div>
     </div>
   )
--- spa/src/pages/list.jsx
+++ spa/src/pages/list.jsx
@@ -2,21 +2,23 @@ import { Map } from "../components/map"
 import { Button } from "../components/button"
 import { NavLink } from "react-router"
 
+
 export function List() {
   return (
-    <div className='bg-slate-300 h-screen p-4 grid grid-cols-2'>
-      <main className='bg-slate-50 p-4 mr-4 rounded-xl'>
-        <div className='flex justify-between'>
-          <h1 className='text-2xl font-bold'>Family Farming</h1>
+    <div className='bg-slate-300 h-screen p-8 grid grid-cols-2 gap-8'>
+      <main className='bg-slate-50 p-8 rounded-xl'>
+        <h1 className='text-2xl font-bold mb-4'>Family Farming</h1>
+        <div className='flex justify-between items-center mb-4 mt-4'>
+          <h1 className='text-lg font-medium text-slate-700'>Agricultores</h1>
           <NavLink to="/">
-            <Button>Voltar</Button>
+            <Button>Voltar</Button> 
           </NavLink>
         </div>
-        <div className='flex flex-col gap-4'>
-          Lista
+        <div className='flex flex-col gap-4 mb-4'>
+          <p>john</p>
+          <p>mary</p>
+          <p>jane</p>
         </div>
-        <Button>Salvar</Button>
-        <p></p>
       </main>
       <div className="min-h-[50vh] rounded-xl">
         <Map />
