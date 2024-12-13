08b2541d8b4f0dab1463cc02ac279337ed80e4ab
José Henrique <jh4832184@gmail.com>
2024-12-13 16:16:05 -0300

    view: add properties onClick into button and onChange, value int input

--- spa/src/components/button.jsx
+++ spa/src/components/button.jsx
@@ -1,8 +1,9 @@
 
-export function Button({children}) {
+export function Button({children, onClick}) {
   return (
     <button
       className="p-1 px-2 border border-gray-300 rounded-md outline-none text-lg"
+      onClick={onClick}
     >
       {children}
     </button>
--- spa/src/components/input.jsx
+++ spa/src/components/input.jsx
@@ -1,9 +1,11 @@
 
-export function Input({placeholder}) {
+export function Input({placeholder, value, onChange}) {
   return (
     <input
       className="p-1 px-2 border border-gray-300 rounded-md outline-none text-lg w-full"
       placeholder={placeholder} 
-      type="text" />
+      type="text"
+      value={value}
+      onChange={onChange} />
   )
 }
