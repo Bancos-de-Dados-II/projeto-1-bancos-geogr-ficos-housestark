9087430f27c6b12c59e036b500e63c00c540b2eb
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-01-06 19:48:29 -0300

    feat: adicionando conexão com postgres

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+/* import express from "express";
 
 const app = express();
 
@@ -8,4 +8,25 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-});
+}); */
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const createAgricultor = async () => {
+  const agricultor = await prisma.agricultor.create({
+    data: {
+      nome: 'João Teste',
+      email: 'joao@example.com',
+      telefone: '123456789',
+      tamanhoTerreno: '10 hectares',
+      posicaoXTerreno: '20.1234', 
+      posicaoYTerreno: '30',
+    },
+  });
+
+  console.log(agricultor);
+};
+
+createAgricultor();
